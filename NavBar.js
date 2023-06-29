7c73b169a00c9f77b6f099f6088d1a10f0bf77cb
Matthew Pendleton <mapendleton37@gmail.com>
2023-06-28 20:45:03 -0500

    fixed media break by changing css for main button to use id and not class

--- NavBar.js
+++ NavBar.js
@@ -1,6 +1,7 @@
 var isGameMode = false;
 let mousePositionListener = null; //temporary as not sure how to remove event listener that has a binded property without this
 const myIntervals = []; //for interval also not sure yet how to better handle this...just want it to work first
+const collisionChecks = [];
 
 function OpenNavButtonMenu (circleID)
 {
@@ -21,6 +22,7 @@ function ToggleHideNavButtons (){
     var buttons = circle.children;
     for (let x of buttons){
         x.classList.toggle('transform-none');
+        x.style.visibility = x.style.visibility == 'hidden' ? 'visible' : 'hidden';
     }
 }
 
@@ -37,13 +39,16 @@ function SetupGun() {
         let id = setInterval(MoveTarget,2000,target);
         myIntervals.push(id);
         console.log('setting interval, id: '+id);
+
+        id = setInterval(CheckCollision,10,bullet,target);
+        console.log('setting collisionCheck, id: '+id);
+        collisionChecks.push(id);
     }
 }
 
 function MoveTarget(target){
     if(isGameMode){
         target.style.setProperty('left',Math.floor(Math.random()*100)+"%");
-        // console.log(target.style.getPropertyValue('left'));
         target.classList.toggle('move');
     }
     else {
@@ -56,7 +61,7 @@ function MoveTarget(target){
     }
 }
 
-function followMouse (id) {
+function FollowMouse (id) {
     el = document.getElementById(id);
     const position = el.getBoundingClientRect();
     el.style.transition = 'none';
@@ -75,10 +80,34 @@ function EndGame() {
         el.classList.remove('transition-none');
         el.style.transform = 'rotate(0deg)';
         el.style.transition = 'all .4s linear';
+        el.children[0].classList.remove('loaded'); //home-button-photo
         document.getElementById('gun-barrel').classList.remove('active');
         
     }
 }
+
+function CheckCollision(el1, el2) {
+    if(isGameMode){
+        var el1Rect = el1.getBoundingClientRect();
+        var el2Rect = el2.getBoundingClientRect();
+      
+        if ((el1Rect.right >= el2Rect.left && el1Rect.left <= el2Rect.right) &&
+            (el1Rect.bottom >= el2Rect.top && el1Rect.top <= el2Rect.bottom)){
+                if(!el1.children[0].classList.contains('caught')){
+                    el1.children[0].classList.toggle('caught');
+                }
+                el2.classList.toggle('move');
+        }
+    } else {
+        console.log('clearing collisionChecks...');
+        collisionChecks.forEach(function(value){
+            console.log('clearing collisionCheck id: '+value);
+            clearInterval(value);
+        });
+        CheckCollision.length = 0;
+    }
+    
+  }
 
 //Listener Callback Methods
 function MousePositionListener(event){
@@ -90,11 +119,17 @@ function MousePositionListener(event){
 
 function BulletListener(event){
     if(!bullet.classList.contains('move') && bullet.offsetLeft == 50){ //can't shoot until it's returned to starting pos
+        let homeButtonPhoto = document.getElementById('home-button-photo');
+        homeButtonPhoto.classList.remove('loaded');
         bullet.classList.toggle('move');
         this.style.setProperty('--mouse-x', event.clientX + "px");
         this.style.setProperty('--mouse-y', event.clientY + "px");
+        bullet.children[0].classList.remove('caught');
         setTimeout(function(){
             bullet.classList.toggle('move');
         }, 1000);
+        setTimeout(function(){
+            homeButtonPhoto.classList.add('loaded');
+        }, 2000);
     }
 }
